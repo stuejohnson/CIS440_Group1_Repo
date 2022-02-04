83d1c81922e8e1772cf5092f7f6c0584062b92df
Stuart <stuart.ethan.johnson@gmail.com>
2022-02-04 15:36:29 -0700

    some format changes and css changes

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
 function loadAccountPage(){
     console.log("loading Account Page");
-    window.location.href = "index.html"
+    window.location.href = "index.html";
+}
+
+function logOut(){
+    console.log("logged out account");
+    //TODO
 }
