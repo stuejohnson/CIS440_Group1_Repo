b5c5d4bc08653e5ddae6d98d3d92de7ff5f8b130
Joshua Ginn <76979135+JoshuaGinn@users.noreply.github.com>
2022-02-05 02:14:21 -0700

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -2,3 +2,21 @@ function loadAccountPage(){
     console.log("loading Account Page");
     window.location.href = "index.html"
 }
+
+function loadLandingPage(){
+    window.location.href = "landingPage.html"
+}
+
+function loadLoginPage() {
+    window.location.href = "login.html"
+}
+
+function openCreateAccount(){
+    var node = document.getElementById('createaccountborder')
+    node.style.visibility = 'visible';
+}
+
+function closeCreateAccount(){
+    var node = document.getElementById('createaccountborder')
+    node.style.visibility = 'hidden';
+}
